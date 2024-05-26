212cb8b257a3700c81786bebc58ca5f3fe24b2b8
mahi <maheswarirsr@gmail.com>
2024-05-26 18:20:51 -0400

    Final commit

--- UserBDD_Bootcamp/src/test/java/runner/TestRunner.java
+++ UserBDD_Bootcamp/src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features", glue= {"stepDefinition"},
+@CucumberOptions(features="src/test/resources/features/post.feature", glue= {"stepDefinition"},
 monochrome = true, plugin = {
 		"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
